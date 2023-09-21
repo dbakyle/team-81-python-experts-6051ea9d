1869210d7bc764d4dff9fb75674b58ef0deefab5
Kyle <dbakyle@gmail.com>
2023-09-21 20:35:58 +0000

    Extended character class

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,5 +1,20 @@
+from levelup.dummy_gamemap import GameMap
+from levelup.position import Position
 class Character:
+    DEFAULT_NAME = "Bob"
     name = ""
-
-    def __init__(self, character_name):
+    startingPos = Position(1,1)
+    currentPos = Position(1,1)
+    
+    def __init__(self, character_name = DEFAULT_NAME):
         self.name = character_name
+
+    def getName(self):
+        return self.name
+
+    def enterMap(self, gamemap):
+        self.gamemap = gamemap
+
+    def move(self, direction):
+        return self.startingPos
+
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,8 +1,34 @@
 from unittest import TestCase
 from levelup.character import Character
+from levelup.dummy_gamemap import GameMap
+from levelup.position import Position
 
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
         ARBITRARY_NAME = "MyName"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+class TestCharacterInitNoName(TestCase):
+    def test_init(self):
+        DEFAULT_NAME = 'Bob'
+        testobj = Character()
+        self.assertEqual(DEFAULT_NAME, testobj.name)
+
+class TestCharacterGetName(TestCase):
+    def test_init(self):
+        DEFAULT_NAME = 'Bob'
+        testobj = Character()
+        self.assertEqual(DEFAULT_NAME, testobj.getName())
+
+class TestCharacterEnterMap(TestCase):
+    def test_enter_map(self):
+        testobj = Character()
+        gamemap = GameMap()
+        testobj.enterMap(gamemap)
+
+class TestCharacterMove(TestCase):
+    def test_move(self):
+        testObj = Character('Bob')
+        startPos = testObj.move('NORTH')
+        assert startPos.getPosition() == (1,1)
