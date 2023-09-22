dbf18e8abc0fce519ff64e6a013b7c200471bc62
Tabish <tabishkhann85@gmail.com>
2023-09-22 15:48:07 +0000

    map commit

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -1,4 +1,5 @@
 from levelup.position import Position
+from levelup.controller import Direction
 
 class Gamemap:
     def __init__(self):
@@ -17,17 +18,20 @@ class Gamemap:
             retval = False
         return retval
 
-    def newposition(self,checkposition,Direction,move):
+    def calculateposition(self,checkposition,move):
             (x,y) = checkposition.getPosition()
-            retval = True
-            if Direction == North:
-                North = y++1
-            if Direction == West:
-                West == x--1
-            if Direction == East:
-                East = x++1
-            if Direction == South:
-                South = y--1
+            
+            if move == Direction.NORTH:
+                y=y+1
+            if move == Direction.WEST:
+                x=x-1
+            if move == Direction.EAST:
+                x=x+1
+            if move == Direction.SOUTH:
+                y=y-1
+
+            newpos=Position(x,y)
+            return newpos
 
 
          
